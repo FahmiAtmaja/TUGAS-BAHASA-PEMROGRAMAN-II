0f9c1cd42cd19652b27f8afef9cf83b1326b0d09
FahmiAtmaja <fahmiira1@gmail.com>
2025-02-27 15:49:53 +0700

    Delete Tugas directory

--- Tugas/LuasPersegiPanjang.py
+++
@@ -1,6 +0,0 @@
-def luas_persegi_panjang(panjang, lebar):
-    return panjang * lebar
-
-panjang = float(input("Masukkan panjang persegi panjang: "))
-lebar = float(input("Masukkan lebar persegi panjang: "))
-print(f"Luas persegi panjang: {luas_persegi_panjang(panjang, lebar)}cm²")
